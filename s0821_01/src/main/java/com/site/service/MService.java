2fd5e6716ec19910bd82ca56e2c4b6d77a99d940
hwk3005 <hwk3005@naver.com>
2025-08-21 18:31:03 +0900

    250821_01 로그인/아웃, 회원리스트, 회원상세보기 (login,mList,index)

---
+++ s0821_01/src/main/java/com/site/service/MService.java
@@ -0,0 +1,19 @@
+package com.site.service;
+
+import java.util.List;
+
+import com.site.dto.Member;
+
+public interface MService {
+
+	// 로그인확인
+	Member findByIdAndPw(String id, String pw);
+
+	// 회원리스트
+	List<Member> fineAll();
+
+	// 회원상세보기
+	Member fineAndId(String id);
+	
+}
+
